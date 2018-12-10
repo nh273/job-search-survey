519ceaddc9c829c69845f66a9d06b5a49f41372d
Phillip Nguyen <nh273@cornell.edu>
2018-12-10 14:27:11 -0500

    bilingual text implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
-import "./App.css";
+import "../css/App.css";
+import Question from "./Question";
+import LanguageProvider from "../LanguageContext";
+import LanguageSwitch from "./LanguageSwitch";
+import { Translatable } from "./Translatable";
 
 class App extends Component {
   render() {
-    return <div className="App">Hellow World</div>;
+    return (
+      <LanguageProvider>
+        <div className="App">
+          <Translatable text={{ en: "Hello World", vi: "Xin chào thế giới" }} />
+          <LanguageSwitch />
+          <Question
+            text={{
+              vi: "Em có thích ăn rau dền không?",
+              en: "Do you like to eat rau den?"
+            }}
+          />
+        </div>
+      </LanguageProvider>
+    );
   }
 }
 
